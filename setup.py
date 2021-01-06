eadf7f4b96be58f60788461e0661a64f17c191db
Viktor Kachuliak <viktor.kachuliak@deliveryhero.at>
2021-01-06 12:53:58 +0100

    fix django version

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='at_gcp_logging',
       author='Delivery Hero',
       packages=['at_gcp_logging'],
       install_requires=[
-            'django>=3.0'
+            'django>=3.0.0'
       ])
