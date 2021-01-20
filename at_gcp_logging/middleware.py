e505ff90d0892d748e3f2964076db8eb05430519
Viktor Kachuliak <viktor.kachuliak@deliveryhero.at>
2021-01-20 12:17:47 +0100

    use new middleware approach

--- at_gcp_logging/middleware.py
+++ at_gcp_logging/middleware.py
@@ -1,8 +1,9 @@
 import json
 import logging
+from http import HTTPStatus
 
 from django.utils import timezone
-from django.utils.deprecation import MiddlewareMixin
+
 from at_gcp_logging import thread_request_context
 
 
@@ -14,44 +15,55 @@ def _get_client_ip(request):
         return request.META.get('REMOTE_ADDR')
 
 
-class CaptureRequestData(MiddlewareMixin):
+class CaptureRequestData:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    def process_request(self, request):
+    def __call__(self, request):
         thread_request_context.set_request_context(
-            user=request.headers['user'],
+            user=request.headers.get('user') or getattr(request.user, 'username', 'anonymous'),
             ip=_get_client_ip(request),
             requested_path=request.get_full_path(),
             method=request.META.get('REQUEST_METHOD'),
             user_agent=request.META.get('HTTP_USER_AGENT')
         )
 
-    def process_response(self, request, response):
-        try:
-            thread_request_context.purge_request_context()
-        finally:
-            return response
+        response = self.get_response(request)
+
+        self._cleanup()
+        return response
+
+    def process_exception(self, request, exception):
+        self._cleanup()
+
+    def _cleanup(self):
+        thread_request_context.purge_request_context()
 
 
-class LogRequestsGCP(MiddlewareMixin):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._request_start_time = None
+class LogRequestsGCP:
+    def __init__(self, get_response):
+        self.get_response = get_response
         self._logger = logging.getLogger(self.__class__.__name__)
+        self._request_start_time = None
 
-    def process_request(self, request):
+    def __call__(self, request):
         self._logger.info('Received request')
         self._request_start_time = timezone.now()
 
-    def process_response(self, request, response):
-        try:
-            td = timezone.now() - self._request_start_time
-            td_in_ms = td.seconds * 1000 + td.microseconds / 1000
-            payload = {
-                'message': 'Request finished',
-                'duration_ms': td_in_ms,
-                'status': response.status_code
-            }
-            self._logger.info(json.dumps(payload))
-        finally:
-            return response
+        response = self.get_response(request)
+
+        self._log_request(response.status_code)
+        return response
+
+    def process_exception(self, request, exception):
+        self._log_request(HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    def _log_request(self, status_code):
+        td = timezone.now() - self._request_start_time
+        td_in_ms = td.seconds * 1000 + td.microseconds / 1000
+        payload = {
+            'message': 'Request finished',
+            'duration_ms': td_in_ms,
+            'status': status_code
+        }
+        self._logger.info(json.dumps(payload))
